1c69ad45ed78d090ef87203c6b4749021e5692ea
Kunj <shahkunj42@gmail.com>
2022-06-09 22:57:01 -0400

    commit tree 884f98c6c5cd1d7fea9225693ca2364e83a22f8b

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+        
+        const input = document.querySelector('#searchByID')
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.textContent = data.title
+            summary.textContent = data.summary
+        });
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
